8185f49b86600bd5d8f14c119774e5f984f95bf5
Muavia Khan <khanmuavia901@gmail.com>
2026-03-17 09:11:48 +0500

    uitiltycode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import dataBaseConnection from "./db/index.js";
 import  dotenv from "dotenv"
+import app from "./app.js";
 
 
  dotenv.config({path:"./.env"})
 dataBaseConnection()
+.then(()=>{
+     app.listen(`${process.env.PORT}|| 5000 `, () => {
+         console.log(`app is running on http://localhost:${process.env.PORT} `)
+     });
+     app.on('error', (err)=>{
+        console.log(err)
+     })
+  
+})
+.catch((error)=>{
+      console.log(error)
+})
---
+++ src/utillty/AsyncHandler.js
@@ -0,0 +1,23 @@
+   const asyncHandler= (func)=> (req, res,next)=>{
+
+              Promise.resolve(func(req,res,next)).catch((err)=> next(err) )
+       
+   }
+
+    export  {asyncHandler}
+ 
+    
+      // const  asyncHandler= (fn)=> async (req, res, next)=> {
+         
+      //   try {
+      //         await fn(req,res,next)
+      //   } catch (error) { 
+                
+             
+      //         res.status(error.code || 500).json({
+      //            status:false,
+      //            message:error.message
+      //         })
+            
+      //   }
+      // }
